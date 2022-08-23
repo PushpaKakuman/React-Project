1a8d6a31d6189bb4b1d67cd3f0a067d06577d140
PushpaKakuman <pushpakakuman@example.com>
2022-08-23 19:21:23 +0530

    react-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header'
+import Fotter from './Components/Fotter'
+import { Container  } from 'react-bootstrap'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <main>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+        <Fotter/>
+      </main>
     </div>
-  );
+  )
+
 }
 
 export default App;
