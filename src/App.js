cb05b132b756901ae0dce524a1feb807d412a444
PushpaKakuman <pushpakakuman@gmail.com>
2022-08-23 21:57:30 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import Header from './Components/Header'
-import Fotter from './Components/Fotter'
-import { Container  } from 'react-bootstrap'
-
-
 
 function App() {
   return (
     <div>
-      <Header/>
-      <main>
-        <Container>
-          <h1>Welcome</h1>
-        </Container>
-        <Fotter/>
-      </main>
+      <h1>Pushpa</h1>
     </div>
   )
 
