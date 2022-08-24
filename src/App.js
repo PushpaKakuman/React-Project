e68810703278f9fa99552b2b538aeffcb6cfa6f0
PushpaKakuman <pushpakakuman@gmail.com>
2022-08-24 11:04:46 +0530

    Components updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Fotter from './components/Fotter';
 
 function App() {
   return (
     <div>
-      <h1>Pushpa</h1>
+      <Header/>
+      <main>
+        <h1>Welcome to Pro shop</h1>
+      </main>
+      <Fotter/>
     </div>
   )
 
---
+++ src/components/Fotter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container,Row,Col } from 'react-bootstrap' 
+
+function Fotter() {
+  return (
+    <fotter>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy;  ECommerce Shop </Col>
+          </Row>
+      </Container>
+    </fotter>
+  )
+}
+
+export default Fotter
